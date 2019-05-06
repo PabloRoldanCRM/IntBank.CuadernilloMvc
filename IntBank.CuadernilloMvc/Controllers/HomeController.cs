582af50fd64a6fd22427c939857e5606e2ab9c8e
Pablo Roldan <cesar.roldan@rhinosytems.mx>
2019-05-06 11:17:34 -0500

    First Commit

---
+++ IntBank.CuadernilloMvc/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntBank.CuadernilloMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return new ViewAsPdf() {
+                PageSize =Rotativa.Options.Size.A4
+            };
+        }
+    }
+}
---
+++ IntBank.CuadernilloMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntBank.CuadernilloMvc.MvcApplication" Language="C#" %>
